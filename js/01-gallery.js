a3bcc5087c545c10ea76193080130a3640b13fd8
Elvira Lemak <elvralema@rambler.ru>
2023-04-19 17:25:15 +0200

    Task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  galleryListRef: document.querySelector(".gallery"),
+};
+
+const galleryListMarkup = createGalleryListMarkup(galleryItems);
+refs.galleryListRef.insertAdjacentHTML("beforeend", galleryListMarkup);
+
+refs.galleryListRef.addEventListener("click", getUrlFromOriginPhoto);
+
+function createGalleryListMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"/>
+        </a>
+    </li>`
+    )
+    .join("");
+}
+
+function getUrlFromOriginPhoto(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const imgUrl = event.target.dataset.source;
+  openModalWindow(imgUrl);
+}
+
+function openModalWindow(imgUrl) {
+  const instance = basicLightbox.create(`
+    <img src="${imgUrl}" width="800" height="600">
+`);
+  //   instance.show();
+  instance.show(() => console.log("lightbox now visible"));
+}
